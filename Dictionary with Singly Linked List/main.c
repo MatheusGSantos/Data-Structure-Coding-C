526b5366bb71a5458b32efe1b783355f5455e216
Matheus_Gama <mths.gm@gmail.com>
2018-08-20 23:19:36 -0300

    Last Added

--- Dictionary with Singly Linked List/main.c
+++ Dictionary with Singly Linked List/main.c
@@ -1,7 +1,7 @@
 /*
 ////////////////////////////////////////////////////////////////////////////////
 This is Linked List Exercise 1: Dictionary delete, insert and search operations
-Matheus Gama dos Santos, 08/12/2018 13:45
+Matheus Gama dos Santos, 08/20/2018 23:19
 ////////////////////////////////////////////////////////////////////////////////
 */
 
@@ -12,6 +12,7 @@ Matheus Gama dos Santos, 08/12/2018 13:45
 
 struct Node
 {
+	char *key;
 	int value;
 	struct Node* next;
 };
@@ -25,121 +26,80 @@ struct CSLList
 };
 
 
-void Insert(struct CSLList *list, int value, int pos)
+void Insert(struct CSLList *list, int value, char* key)
 {
 	if ( !(list->head) )	//If empty
 	{
 		list->head = (struct Node*) malloc( sizeof(struct Node) );
 		list->head->next = list->head;
 		list->head->value = value;
+		list->head->key = key;
 		list->tail = list->head;
 		list->size++;
 	}
 	else
 	{
-		if ((pos > list->size) || (pos < 0))
-		{
-			printf("Invalid Position.\n");
-		}
-		else
-		{
-			struct Node* Aux;
-			if (pos == 0)		//Prepend operation
-			{
-				Aux = (struct Node*) malloc(sizeof(struct Node));
-				Aux->next = list->head;
-				Aux->value = value;
-				list->head = Aux;
-				list->tail->next = list->head;
-				list->size++;
-			}
-			else
-			{
-				if (pos == list->size)		//Append operation
-				{
-					list->tail->next = (struct Node*) malloc(sizeof(struct Node));
-					list->tail->next->next = list->head;
-					list->tail->next->value = value;
-					list->tail = list->tail->next;
-					list->size++;
-				}
-				else		//insert operation
-				{
-					Aux = list->head;
-					for (int i = 1; i < pos; i++)	//won't happen if pos = 1 
-					{
-						Aux = Aux->next;
-					}
-					struct Node* AdAux = Aux->next;
-					Aux->next = (struct Node*) malloc(sizeof(struct Node));
-					Aux->next->next = AdAux;
-					Aux->next->value = value;
-					list->size++;
-				}
-			}
-		}
+		struct Node* Aux = (struct Node*) malloc(sizeof(struct Node));
+		Aux->next = list->head;
+		Aux->value = value;
+		Aux->key = key;
+		list->head = Aux;
+		list->tail->next = list->head;
+		list->size++;
 	}
 }
 
 
-void Delete(struct CSLList *list, int pos)
+void Delete(struct CSLList *list, struct Node* n)
 {
-	if (!(list->head))	//If empty
+	if (!n)	//If empty
 	{
-		printf("Invalid Operation! Empty List.\n");
+		printf("Invalid operation! Element not found.\n");
 		exit(1);
 	}
 	else
 	{
-		if ( (pos > list->size - 1) || (pos < 0))
+		if (n == list->head)	//deleting first element
 		{
-			printf("Invalid Position.\n");
+			list->tail->next = list->head->next;
+			list->head = list->tail->next;
+			list->size--;
 		}
 		else
 		{
-			if (pos == 0)	//deleting first element
+			struct Node* Aux = list->head;
+			while(Aux->next != n)
 			{
-				list->tail->next = list->head->next;
-				list->head = list->tail->next;
+				Aux = Aux->next;
+			}
+			if (Aux->next == list->tail)	//deleting last element
+			{
+				list->tail = Aux;
+				list->tail->next = list->head;
 				list->size--;
 			}
 			else
 			{
-				struct Node* Aux = list->head;
-				for (int i = 1; i < pos; i++)	//won't happen if pos = 1 
-				{
-					Aux = Aux->next;
-				}
-
-				if (pos == (list->size - 1))	//deleting last element
-				{
-					list->tail = Aux;
-					list->tail->next = list->head;
-					list->size--;
-				}
-				else
-				{
-					Aux->next = Aux->next->next;
-					list->size--;
-				}
+				Aux->next = Aux->next->next;
+				list->size--;
 			}
 		}
 	}
 }
 
 
-int Search(struct CSLList *list, int value)
+struct Node* Search(struct CSLList *list, const char* key)
 {
 	struct Node* Aux = list->head;
 	for (int i = 0; i < list->size; i++)
 	{
-		if (Aux->value == value)
+		if (!strcmp(Aux->key,key))
 		{
-			return i;	//return index
+			return Aux;	//returns pointer to element
 		}
 		Aux = Aux->next;
 	}
-	return -1;	 //No matches
+	return NULL;	 //No matches
 }
 
 
@@ -148,7 +108,7 @@ void PrintList(struct CSLList list)
 	struct Node* Aux = list.head;
 	for (int i = 0; i < list.size; i++)
 	{
-		printf("List[%d]: %d\n",i ,Aux->value);
+		printf("%s : %d\n", Aux->key ,Aux->value);
 		Aux = Aux->next;
 	}
 }
